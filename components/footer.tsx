da34384f887339aaa16cc37f9cbf394031552fc8
Dishit79 <dishit79@gmail.com>
2025-06-23 15:16:48 -0400

    added location

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,7 +3,9 @@ export function Footer() {
     <footer className="border-t mt-16">
       <div className="container mx-auto px-4 py-8 max-w-4xl">
         <div className="flex items-center justify-between text-sm text-muted-foreground">
-          <p> 🫎 moose</p>
+          <p> 🫎 moose -
+            <i> ottawa, toronto</i>
+          </p>
           <p>2025 Nawaf Ahmed</p>
         </div>
       </div>
